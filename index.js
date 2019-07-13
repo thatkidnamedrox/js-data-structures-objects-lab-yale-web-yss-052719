9df5f79a6226174f333e76e31a207986f4d2a8da
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-07-13 14:29:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value)
 {
-  result = {};
+  let result = {};
   for (const key in driver) {
     result[key] = driver[key];
   }
